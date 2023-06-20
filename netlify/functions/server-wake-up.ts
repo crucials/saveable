a874cfaf66525c83270330834128b2f496b46fd8
crucials <ensueness@gmail.com>
2023-06-20 09:18:02 +0500

    Added server-wake-up cron function

---
+++ netlify/functions/server-wake-up.ts
@@ -0,0 +1,15 @@
+import { Handler, HandlerEvent, HandlerContext, schedule } from '@netlify/functions'
+import fetch from 'node-fetch'
+import { SERVER_BASE_URL } from '~~/constants/api-urls'
+
+const scheduledHandler : Handler = async (event: HandlerEvent, context: HandlerContext) => {
+   await fetch(SERVER_BASE_URL)
+
+   return {
+      statusCode: 200,
+   }
+}
+
+const handler = schedule('*/10 * * * *', scheduledHandler)
+
+export { handler }
