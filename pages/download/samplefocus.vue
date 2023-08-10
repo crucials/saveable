3a42d0fd57aad09344bf03e470da997f3156968a
crucials <ensueness@gmail.com>
2023-08-10 10:24:21 +0500

    Added samplefocus downloader functionality

---
+++ pages/download/samplefocus.vue
@@ -0,0 +1,94 @@
+<template>
+    <NuxtLayout name="service-page">
+        <template #form>
+            <form class="service-form" @submit.prevent="download">
+                <ServicePageHeading>
+                    Download from SampleFocus
+                </ServicePageHeading>
+
+                <IconTextField type="url" placeholder="Sample page link" v-model.trim="sampleLink"
+                    class="link-field">
+                    <img src="~~/assets/images/link.svg" alt="Two paperclips, link icon">
+                </IconTextField>
+
+                <LoadingButton :loading="loading">
+                    Download
+                </LoadingButton>
+
+                <ErrorText :visible="errorTextVisible">
+                    {{ errorText }}
+                </ErrorText>
+            </form>
+        </template>
+
+        <template #icon>
+            <img src="~~/assets/images/samplefocus.svg" 
+                alt="'Sample' and 'Focus' labels divided by sound wave, SampleFocus logo"
+                class="page-icon">
+        </template>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+    import { DEFAULT_SERVER_DOWNLOAD_ERROR_MESSAGE } from '~/constants/messages'
+    import JsFileDownloader from 'js-file-downloader'
+    import { MediaInfo } from '~/types'
+
+    useHead({
+        title: 'Download from SampleFocus | Saveable'
+    })
+
+    const { errorTextVisible, errorText, showErrorText } = useErrorText()
+
+    const sampleLink = ref('')
+    const loading = ref(false)
+
+    async function download() {
+        const sampleLinkValue = sampleLink.value
+
+        if(sampleLinkValue.length < 1) {
+            showErrorText('You should enter sample link to download it')
+        }
+        else {
+            try {
+                loading.value = true
+
+                const response = await fetch(`/api/media-info/samplefocus?sample_url=${sampleLinkValue}`)
+
+                if(!response.ok) {
+                    if(response.status === 400) {
+                        const error = await response.json()
+                        showErrorText(error.message)
+                    }
+                    else if(response.status === 500) {
+                        showErrorText('Failed to grab sample info. If your URL is valid, then, apparently, ' 
+                            + 'SampleFocus updated their website', 3500)
+                    }
+                    else {
+                        showErrorText(DEFAULT_SERVER_DOWNLOAD_ERROR_MESSAGE)
+                    }
+
+                    loading.value = false
+                }
+                else {
+                    const sampleInfo : MediaInfo = await response.json()
+
+                    await new JsFileDownloader({ 
+                        url: `/api/proxy/media?url=${encodeURIComponent(sampleInfo.downloadUrl)}`, 
+                        filename: sampleInfo.name + '.mp3'
+                    })
+
+                    loading.value = false
+                }
+            }
+            catch(error) {
+                showErrorText(DEFAULT_SERVER_DOWNLOAD_ERROR_MESSAGE)
+                loading.value = false
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ server/api/media-info/samplefocus.ts
@@ -0,0 +1,65 @@
+import { HTMLElement, parse } from 'node-html-parser'
+import { MediaInfo } from '~/types'
+
+interface Sample {
+    slug : string,
+    sample_mp3_url : string
+}
+
+const downloadMethodErrorMessage = 'Failed to grab sample info. ' + 
+    'Make sure your URL starts with \'https://samplefocus.com/samples/...\'. ' +
+    'If URL is valid, apparently, SampleFocus updated their website. ' + 
+    'Hit me up on Discord - @amputating or open an issue on Github - ' + 
+    'https://github.com/crucials/saveable'
+
+export default defineEventHandler<MediaInfo>(async event => {
+    const sampleUrl = getQuery(event)['sample_url']?.toString()
+
+    if(!sampleUrl) {
+        throw createError({
+            statusCode: 400,
+            message: `Query parameter 'sample_url' not provided`
+        })
+    }
+
+    let root : HTMLElement
+    try {
+        const samplePageResponse = await fetch(sampleUrl)
+
+        if(!samplePageResponse.ok) {
+            throw new Error()
+        }
+
+        root = parse(await samplePageResponse.text())
+    }
+    catch(error) {
+        throw createError({
+            statusCode: 400,
+            message: 'Failed to parse sample page. Make sure URL is valid and requested sample exists',
+        })
+    }
+
+    const sampleWaveForm = root.querySelectorAll('div[data-react-class="SampleWaveform"]')[0]
+    const rawSamplesJson = sampleWaveForm?.getAttribute('data-react-props')
+
+    if (!sampleWaveForm || !rawSamplesJson) {
+        throw createError({
+            statusCode: 500,
+            message: downloadMethodErrorMessage,
+        })
+    }
+
+    const sample: Sample = JSON.parse(rawSamplesJson)?.sample
+
+    if (!sample) {
+        throw createError({
+            statusCode: 500,
+            message: downloadMethodErrorMessage,
+        })
+    }
+
+    return {
+        downloadUrl: sample.sample_mp3_url,
+        name: sample.slug,
+    }
+})
