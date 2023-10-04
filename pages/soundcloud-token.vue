fc5fbea580e550919ad37552e65d9806cbe2f37d
crucials <ensueness@gmail.com>
2023-10-04 20:12:19 +0500

    created soundcloud token info page, started creating banner

---
+++ pages/soundcloud-token.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="article-banner">
+        <Heading>
+            What’s a “SoundCloud account token” and why this app needs it
+        </Heading>
+    </header>
+</template>
+
+<script setup lang="ts">
+    useHead({
+        title: 'What’s a “SoundCloud account token” and why this app needs it | Saveable'
+    })
+</script>
+
+<style lang="scss">
+    .article-banner {
+        padding: 30px;
+        height: 164px;
+        background: var(--amethyst, linear-gradient(101deg, #FFF 0%, #11998E 0.01%, #A58DE9 0.02%, #38EF7D 100%));
+    }
+</style>
