f077139c4457748f8965d7e937092840bdc6daaa
crucials <ensueness@gmail.com>
2023-10-07 16:01:21 +0500

    added theme-based color option to heading and changed color applying logic

--- components/ui-kit/heading.vue
+++ components/ui-kit/heading.vue
@@ -2,7 +2,7 @@
     <Component :is="type" class="service-page-heading" 
         :class="{ 
             'smaller-margin-bottom': smallerMarginBottom,
-            'pastel-purple-text': color === 'pastel-purple'
+            [color]: true
         }">
         <slot></slot>
     </Component>
@@ -12,7 +12,7 @@
     withDefaults(defineProps<{
         smallerMarginBottom? : boolean,
         type? : 'h1' | 'h2' | 'h3',
-        color? : 'white' | 'pastel-purple'
+        color? : 'white' | 'pastel-purple' | 'black-and-white'
     }>(), {
         type: 'h1',
         color: 'white'
@@ -21,13 +21,16 @@
 
 <style scoped lang="scss">
     .service-page-heading {
-        color: white;
+        @extend %themeable;
         font-weight: 700;
-        font-size: 2rem;
 
         &:not(:last-child) {
             margin-bottom: 41px;
         }
+    }
+
+    h1.service-page-heading {
+        font-size: 2rem;
 
         @media (max-width: 1200px) {
             font-size: 1.6rem;
@@ -38,8 +41,32 @@
         }
     }
 
-    .pastel-purple-text {
+    h2.service-page-heading {
+        font-size: 1.72rem;
+
+        @media (max-width: 1200px) {
+            font-size: 1.4rem;
+        }
+
+        @media (max-width: 600px) {
+            font-size: 1.22rem;
+        }
+    }
+
+    .pastel-purple {
         color: $pastel-purple;
+    }
+
+    .white {
+        color: white;
+    }
+
+    .black-and-white {
+        color: black;
+
+        .dark & {
+            color: white;
+        }
     }
 
     .service-page-heading.smaller-margin-bottom:not(:last-child) {
