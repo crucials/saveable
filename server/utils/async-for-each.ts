79c4a21596198637f25ebc53864dcc9823e1cb04
crucials <ensueness@gmail.com>
2023-10-12 12:31:45 +0500

    added async for-each utility

---
+++ server/utils/async-for-each.ts
@@ -0,0 +1,14 @@
+export default function asyncForEach<TElement>(
+    array : TElement[],
+    doForEach : (value : TElement, index : number) => Promise<void>
+) {
+    return new Promise<void>(resolve => {
+        array.forEach(async (element, index) => {
+            await doForEach(element, index)
+
+            if(index === array.length - 1) {
+                resolve()
+            }
+        })
+    })
+}
