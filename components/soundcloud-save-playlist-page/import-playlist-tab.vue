5abb9f8a45c086806b35ffe1eb5d715ae863d685
crucials <ensueness@gmail.com>
2023-10-15 14:59:44 +0500

    almost finished import page

--- components/soundcloud-save-playlist-page/import-playlist-tab.vue
+++ components/soundcloud-save-playlist-page/import-playlist-tab.vue
@@ -1,22 +1,31 @@
 <template>
     <NuxtLayout name="service-page">
         <template #form>
-            <form class="service-form" @submit.prevent="">
+            <form class="service-form" @submit.prevent="importPlaylist">
                 <Heading smaller-margin-bottom>
                     Import playlist from JSON
                 </Heading>
 
                 <p class="white-text">
-                    Creates new playlist on your account based on JSON data generated on export playlist 
-                    page
+                    Creates new playlist on your account identical to playlist from JSON data 
+                    generated on export playlist page
                 </p>
 
-                <FileUploadArea @file-uploaded="(file : File) => uploadedFile = file" class="video-upload-area">
-                    Upload playlist data
+                <FileUploadArea @file-uploaded="(file : File) => uploadedFile = file" 
+                    accept="application/json">
+                    Upload JSON file
                     (<mark class="white-text">click</mark>
                     or
                     <mark class="white-text">drag and drop</mark>)
                 </FileUploadArea>
+
+                <IconTextField placeholder="Account token" v-model.trim="accountToken" 
+                    full-width>
+                    <NuxtLink to="/soundcloud-token" title="Learn about SoundCloud token">
+                        <img src="~~/assets/images/info.svg" class="token-field-info-icon" 
+                            alt="Letter 'i' placed in a rounded square, information icon">
+                    </NuxtLink>
+                </IconTextField>
 
                 <div class="actions">
                     <LoadingButton :loading="loading">
@@ -30,6 +39,13 @@
 
                 <ErrorText :visible="errorTextVisible">
                     {{ errorText }}
+                </ErrorText>
+
+                <ErrorText :visible="noTokenErrorVisible">
+                    You need to enter your account token so app can create playlist on your account.
+                    <NuxtLink to="/soundcloud-token" class="action-text">
+                        Click here to learn more
+                    </NuxtLink>
                 </ErrorText>
             </form>
         </template>
@@ -53,10 +69,54 @@
 </template>
 
 <script setup lang="ts">
-const { errorText, errorTextVisible, showErrorText } = useErrorText()
-const loading = ref(false)
-
 const emit = defineEmits<{
     (event : 'tab-switched', tabNumber: number): void
 }>()
+
+const { errorText, errorTextVisible, showErrorText } = useErrorText()
+const { 
+    errorTextVisible: noTokenErrorVisible, 
+    showErrorText: showNoTokenError 
+} = useErrorText()
+const loading = ref(false)
+
+const accountToken = ref('')
+let uploadedFile : File | undefined = undefined
+
+async function importPlaylist() {
+    if(uploadedFile) {
+        if(uploadedFile.type !== 'application/json') {
+            showErrorText('File must be a JSON format')
+            return
+        }
+
+        if(!accountToken.value) {
+            showNoTokenError()
+            return
+        }
+
+        try {
+            loading.value = true
+
+            
+        }
+        catch(error) {
+            showErrorText('Something went wrong while importing')
+        }
+    }
+    else {
+        showErrorText('You need to upload a JSON file to restore playlist from it')
+    }
+}
 </script>
+
+<style lang="scss" scoped>
+.token-field-info-icon {
+    display: block;
+    transition: scale 0.3s ease;
+
+    &:hover {
+        scale: 1.15;
+    }
+}
+</style>
