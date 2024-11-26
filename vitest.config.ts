226f1f890bde07cb3c51c6199c4e7c18157ff5c8
crucials <ensueness@gmail.com>
2024-11-26 11:06:09 +0500

    delete already deleted file `setup.ts` from vite cfg

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
     test: {
-        setupFiles: './tests/setup.ts',
         environment: 'jsdom',
     },
 })
